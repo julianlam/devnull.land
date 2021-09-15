5e55c4133ad2aba7cae1474e38278bcdbf5aed04
Julian Lam <julian@nodebb.org>
2021-09-15 10:07:03 -0400

    feat: undo eslint@8, move gists methods to own lib

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 /* globals import */
 
-import fetch from 'node-fetch';
 import express from 'express';
 import benchpress from 'benchpressjs';
 import path, { dirname } from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
+import { _gists, check, get } from './lib/gists.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const fsPromises = fs.promises;
+const fsPromises = fs.promises
 
 const app = express();
 const port = 3000;
@@ -26,46 +26,9 @@ app.engine('jst', benchpress.__express);
 app.set('view engine', 'jst');
 app.set('views', path.join(viewsDir, 'build'));
 
-// TODO: Move this to a separate lib
-const _gists = new Map();
-let _gistsETag;
-const refreshGists = async () => {
-	let gists = await fetch('https://api.github.com/users/julianlam/gists');
-	_gistsETag = gists.headers.get('etag');
-	gists = await gists.json();
+await get();
 
-	// Filter out non-blog gists
-	gists = gists.filter(gist => gist.description.indexOf('#blog') !== -1);
-
-	_gists.clear();
-	await Promise.all(gists.map(async (gist) => {
-		const name = Object.keys(gist.files).pop();
-		const markdown = await (await fetch(gist.files[name].raw_url)).text();
-		_gists.set(name, {
-			name,
-			markdown,
-			title: gist.description,
-			created_at: gist.created_at,
-			timestamp: new Date(gist.created_at).getTime(),
-		});
-	}));
-};
-const checkGists = async (req, res, next) => {
-	const response = await fetch('https://api.github.com/users/julianlam/gists', {
-		method: 'head',
-	});
-	const etag = response.headers.get('etag');
-
-	if (etag !== _gistsETag) {
-		await refreshGists();
-	}
-
-	next();
-};
-
-await refreshGists();
-
-app.get('/', checkGists, async (req, res) => {
+app.get('/', check, async (req, res) => {
 	const gists = [];
 	for (const [filename, gist] of _gists) {
 		gist.url = filename.slice(0, -3);
@@ -76,7 +39,7 @@ app.get('/', checkGists, async (req, res) => {
 	res.render('index', { gists });
 });
 
-app.get('/:title', checkGists, async (req, res) => {
+app.get('/:title', check, async (req, res) => {
 	const gist = _gists.get(`${req.params.title}.md`);
 	res.render('post', gist);
 });
